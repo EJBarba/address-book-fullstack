9f68a9f7619746d51b7b4fd714b7f4021dd8d9c4
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-07-30 13:43:16 +0800

    Backend -- completed login/registration

--- migrations/1564393325501_users-table.js
+++ migrations/1564393325501_users-table.js
@@ -1,20 +1,24 @@
 exports.shorthands = undefined;
 
-// exports.up = pgm => {
-//     pgm.createTable('users', {
-//       id: {
-//         type: 'serial',
-//         primaryKey: true,
-//       },
-//       email: {
-//         type: 'text',
-//         notNull: true,
-//       },
-//       password: {
-//         type: 'text',
-//         notNull: true,
-//       },
-//     });
-//   };
+exports.up = pgm => {
+  pgm.createTable("users", {
+    id: {
+      type: "serial",
+      primaryKey: true
+    },
+    username: {
+      type: "text",
+      primaryKey: true
+    },
+    email: {
+      type: "text",
+      notNull: true
+    },
+    password: {
+      type: "text",
+      notNull: true
+    }
+  });
+};
 
 exports.down = pgm => {};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const massive = require("massive");
+const users = require("./controllers/users.js");
 
-const app = express();
+massive({
+  host: "localhost",
+  port: 5432,
+  database: "addressbook",
+  user: "postgres",
+  password: "addressbook"
+}).then(db => {
+  const app = express();
+  const port = 3001;
 
-const port = 3001;
+  app.set("db", db);
+  app.use(express.json());
 
-app.get("/api/products", (req, res) => {
-  res.status(200).send("full stack is lit");
-});
+  app.get("/api/debug", users.debug);
+  app.post("/api/register", users.register);
+  app.post("/api/login", users.login);
 
-app.listen(port, () => {
-  console.log(`Server listening (0  0 3) on port: ${port}`);
+  app.listen(port, () => {
+    console.log(`Server listening (0  0 3) on port: ${port}`);
+  });
 });
